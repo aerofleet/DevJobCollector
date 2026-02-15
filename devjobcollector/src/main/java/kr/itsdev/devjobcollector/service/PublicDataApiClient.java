95a410e5ccdf5c07f5003a00395f356fad59a85e
Hong SH <aerofleet72@gmail.com>
2026-02-16 01:42:10 +0900

    refactor: 공공데이터 API 날짜 파싱 로직 개선 및 안정화
    - PublicJobDto 내 String 날짜를 LocalDate로 변환하는 parseDate 메서드 구현
    - 날짜 데이터 누락 또는 형식 불일치 시 예외 처리 추가
    - API 응답 데이터 매핑 로직 정교화로 데이터 무결성 향상
    - 시작일(pbancBgngYmd) 및 종료일 필드 참조 오류 수정

--- devjobcollector/src/main/java/kr/itsdev/devjobcollector/service/PublicDataApiClient.java
+++ devjobcollector/src/main/java/kr/itsdev/devjobcollector/service/PublicDataApiClient.java
@@ -1,8 +1,8 @@
-﻿package kr.itsdev.devjobcollector.service;
+package kr.itsdev.devjobcollector.service;
 
 import kr.itsdev.devjobcollector.config.PublicDataProperties;
 import kr.itsdev.devjobcollector.dto.PublicDataDetailResponse;
-import kr.itsdev.devjobcollector.dto.PublicDataResponse;
+import kr.itsdev.devjobcollector.dto.PublicDataListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -26,7 +26,7 @@ public class PublicDataApiClient {
         /**
          * 공공데이터 목록 API를 호출한다. [/list]
          */
-        public PublicDataResponse fetchJobList(int page, int size) {
+        public PublicDataListResponse fetchJobList(int page, int size) {
             URI uri = UriComponentsBuilder.fromUriString(properties.getBaseUrl() + "/list")
                     .queryParam("serviceKey", properties.getServiceKey())
                     .queryParam("numOfRows", size)
@@ -35,7 +35,7 @@ public class PublicDataApiClient {
                     .build(true) // 서비스키 인코딩 문제 방지를 위해 true 설정
                     .toUri();
 
-            return restTemplate.getForObject(uri, PublicDataResponse.class);
+            return restTemplate.getForObject(uri, PublicDataListResponse.class);
         }
 
         /**
