65cf979edbe77a7b79e74b154119e9a7c9425779
Hong SH <aerofleet72@gmail.com>
2026-02-16 01:22:56 +0900

    수정 - 목록 조회용 Dto (private List<String> techStacks; //추가)

--- devjobcollector/src/main/java/kr/itsdev/devjobcollector/dto/JobPostDto.java
+++ devjobcollector/src/main/java/kr/itsdev/devjobcollector/dto/JobPostDto.java
@@ -1,18 +1,31 @@
-﻿package kr.itsdev.devjobcollector.dto;
+package kr.itsdev.devjobcollector.dto;
 
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class JobPostDto {
-    private String title;
-    private String companyName;
-    private String location;
+
+    private Long id;    
     private String sourcePlatform;
-    // 필요한 필드가 더 있다면 여기에 추가하세요 (startDate, endDate 등)
+    private String companyName;
+    private String title;
+    private String jobCategory;
+    private String location;
+    private String hireType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String originalUrl;
+    private boolean isActive;
+
+    // 기술 스택 목록
+    private List<String> techStacks;
+    
 }
