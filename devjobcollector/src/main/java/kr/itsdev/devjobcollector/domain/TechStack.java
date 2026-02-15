18f053eb01590db79195a1316492740bbaeacee4
Hong SH <aerofleet72@gmail.com>
2026-02-16 01:21:23 +0900

    수정 - 이중 매핑 제거(@ManyToMany + @ManyToOne 삭제), @OneToMany관계로 변경(PostTag 사용)

--- devjobcollector/src/main/java/kr/itsdev/devjobcollector/domain/TechStack.java
+++ devjobcollector/src/main/java/kr/itsdev/devjobcollector/domain/TechStack.java
@@ -1,27 +1,44 @@
-﻿package kr.itsdev.devjobcollector.domain;
+package kr.itsdev.devjobcollector.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 기술 스택 마스터 테이블
+ * post_tags 중간 테이블을 통해 job_posts와 N:M 관계
+ */
 @Entity
-@Table(name = "teck_stacks")
+@Table(
+    name = "tech_stacks",
+    indexes = @Index(name = "idx_stack_name", columnList = "stack_name")
+)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
+@ToString(exclude = "postTags")
+@EqualsAndHashCode(of = "stackName")
 public class TechStack {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(unique = true, nullable = false, length = 50)
+    @Column(name = "stack_name", unique = true, nullable = false, length = 50)
     private String stackName;       //기술 명칭 (예: Java, React, MySQL)
 
-    @ManyToMany(mappedBy = "techStacks")
-    @Builder.Default
-    private List<JobPost> jobPosts = new ArrayList<>();
+    @OneToMany(mappedBy = "techStack", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PostTag> postTags = new ArrayList<>();
 
+    @Builder
+    public TechStack(String stackName){
+        this.stackName = stackName;
+    }
+
+    public void addPostTag(PostTag postTag){
+        postTags.add(postTag);
+        if (postTag.getTechStack() != this){
+            postTag.setTechStack(this);
+        }
+    }
 }
