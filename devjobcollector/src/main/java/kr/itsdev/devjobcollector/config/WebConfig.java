7f45020e6060f108ec749d6e5af8c7fe6df6aec7
Hong SH <aerofleet72@gmail.com>
2026-02-25 01:17:07 +0900

    Page 직렬화 VIA_DTO 적용으로 안정적 페이지 응답

--- devjobcollector/src/main/java/kr/itsdev/devjobcollector/config/WebConfig.java
+++ devjobcollector/src/main/java/kr/itsdev/devjobcollector/config/WebConfig.java
@@ -1,11 +1,14 @@
 package kr.itsdev.devjobcollector.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
