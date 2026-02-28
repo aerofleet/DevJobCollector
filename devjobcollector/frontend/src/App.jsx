0a846e0ec694fdf440bc0096d24be38a2f5e24c0
Hong SH <aerofleet72@gmail.com>
2026-03-01 04:04:00 +0900

    상세페이지에서 헤더 검색이 안되는 오류 수정

--- devjobcollector/frontend/src/App.jsx
+++ devjobcollector/frontend/src/App.jsx
@@ -1,28 +1,36 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import DetailPage from './pages/DetailPage';
 import './styles/App.css';
 import ScrollToTop from './components/common/ScrollToTop';
 import Header from './pages/Header';
 
-function App() {
+const AppRoutes = () => {
   const [searchParams, setSearchParams] = useState({ keyword: '' });
+  const navigate = useNavigate();
 
   const handleSearch = (keyword = '') => {
     setSearchParams({ keyword });
+    navigate('/');
   };
 
   return (
+    <div className="app">
+      <Header onSearch={handleSearch} />
+      <Routes>
+        <Route path="/" element={<MainPage searchParams={searchParams} />} />
+        <Route path="/job/:id" element={<DetailPage />} />
+      </Routes>
+      <ScrollToTop />
+    </div>
+  );
+};
+
+function App() {
+  return (
     <BrowserRouter>
-      <div className="app">
-        <Header onSearch={handleSearch} />
-        <Routes>
-          <Route path="/" element={<MainPage searchParams={searchParams} />} />
-          <Route path="/job/:id" element={<DetailPage />} />
-        </Routes>
-        <ScrollToTop />
-      </div>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
